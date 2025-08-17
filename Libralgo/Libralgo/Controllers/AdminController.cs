9e6d832442dc18605fa67f16fa6a46268dd4d3da
Yavuz Yazici <4yavuzyazici@gmail.com>
2025-08-17 12:35:01 +0300

    Admin Layout ve Dashboard temsili olarak oluşturuldu

---
+++ Libralgo/Libralgo/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Libralgo.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        [Route("/admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
